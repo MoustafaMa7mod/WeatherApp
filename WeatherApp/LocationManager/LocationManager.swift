d93714de1e5f1bafd1f1a62f77ff837783b3b0d7
MoustafaMa7mod <mostafa.akrap@gmail.com>
2025-02-09 12:52:31 +0200

    access user location

---
+++ WeatherApp/LocationManager/LocationManager.swift
@@ -0,0 +1,37 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Mostafa Mahmoud on 09/02/2025.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationPermission:NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var authorizationStatus : CLAuthorizationStatus = .notDetermined
+    private let locationManager = CLLocationManager()
+    @Published var coordinates : CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func requestLocationPermission()  {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        
+        coordinates = location.coordinate
+    }
+}
---
+++ WeatherApp/Presentation/HomeView/ViewModel/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+// HomeViewModel.swift
+//  WeatherApp
+//
+//  Created by Mostafa Mahmoud on 09/02/2025.
+//
+
+import Foundation
+//import Domain
+//import Data
+
+final class HomeViewModel: ObservableObject {
+
+}
